65614a2dd4fb2f4e873fc7798932f6f8c11e0bc5
pirenage <81282992+pirenage@users.noreply.github.com>
2024-03-23 09:50:21 +0700

    Create Upload.php

---
+++ app/Controllers/Upload.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Upload extends BaseController {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('image_model');
+    }
+
+    public function index() {
+        $this->load->view('index');
+    }
+
+    public function upload_image() {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'jpg|jpeg|png|gif';
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload('image')) {
+            $data = $this->upload->data();
+            $image_path = 'uploads/' . $data['file_name'];
+            $this->image_model->insert_image($image_path);
+            $this->load_gallery();
+        } else {
+            echo 'error';
+        }
+    }
+
+    public function load_gallery() {
+        $data['images'] = $this->image_model->get_images();
+        $this->load->view('gallery', $data);
+    }
+}
+?>
